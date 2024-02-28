4998a588f0fd889f3b271ab490245de42b449ef9
Maverich121 <mrtgrgl007@gmail.com>
2024-02-28 09:44:13 +0300

    Mert Gorgulu

---
+++ main.py
@@ -0,0 +1,12 @@
+# Swapping two numbers using temporary variable
+a = 56
+b = 123
+
+print("Before swapping: a =", a, ", b =", b)
+
+# Using temporary variable
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, ", b =", b)
